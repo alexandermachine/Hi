9de8d632c3b974c62571ba495edeba5ec6e73c8d
alexandermachine <alexanderuv@mail.ru>
2017-01-16 07:55:22 +0600

    Initial commit

---
+++ src/ru/AV/HelloWorld/HelloWorld.java
@@ -0,0 +1,10 @@
+package ru.AV.HelloWorld;
+
+/**
+ * Created by Alexander V on 16.01.2017.
+ */
+public class HelloWorld {
+    public static void main(String[] args){
+        System.out.println("Hello World!!!");
+    }
+}
